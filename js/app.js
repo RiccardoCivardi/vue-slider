8a01d14be07d66f44470b2a423ee253592e51c00
RiccardoCivardi <riccardo.civardi@icloud.com>
2022-11-03 15:35:39 +0100

    [add] nextPrev @click and @click on thumbnails

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
 /*
-Esercizio di oggi: Vue Slider
-nome repo: vue-slider
-Descrizione:
-Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
-- al click su una thumb, visualizzare in grande l’immagine corrispondente
-- aggiungere la classe active alla thumb attiva
-Bonus:
+
 1- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
 2- quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
 Consigli del giorno:
@@ -14,30 +8,62 @@ Consigli del giorno:
 Buon lavoro e buon divertimento!
 */
 
-const images = [
-  {
-   title: 'Svezia',
-   description: 'Una vita senza amore è come un anno senza estate',
-   url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg'
+
+const {createApp} = Vue;
+
+createApp({
+  
+  data(){
+    return{
+
+      images: [
+        {
+         title: 'Svezia',
+         description: 'Una vita senza amore è come un anno senza estate',
+         url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg'
+        },
+        {
+         title: 'Perù',
+         description: 'Signora Presidente, il Perù è sempre stata la nostra pecora nera in America latina',
+         url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg'
+        },
+        {
+         title: 'Chile',
+         description: 'Il Cile è un paese maschilista: l\'aria è talmente densa di testosterone che è un miracolo se alle donne non spuntano i peli in faccia',
+         url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c'
+        },
+        {
+         title: 'Argentina',
+         description: 'La storia di Buenos Aires sta scritta nel suo elenco telefonico',
+         url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg'
+        },
+        {
+         title: 'Colombia',
+         description: 'In Colombia avere le allucinazioni è una normalità',
+         url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop'
+        }
+      ],
+
+      counter: 0
+
+    }
   },
-  {
-   title: 'Perù',
-   description: 'Signora Presidente, il Perù è sempre stata la nostra pecora nera in America latina',
-   url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg'
+
+  methods: {
+
+    // funzione per bottini avanti e indietro
+    nextPrev(isNext){
+      isNext ? this.counter++ : this.counter--;
+      // condizioni per loop infinito 
+      if(this.counter < 0) this.counter = this.images.length - 1;
+      if(this.counter === this.images.length) this.counter = 0;
+    }
+
   },
-  {
-   title: 'Chile',
-   description: 'Il Cile è un paese maschilista: l\'aria è talmente densa di testosterone che è un miracolo se alle donne non spuntano i peli in faccia',
-   url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c'
-  },
-  {
-   title: 'Argentina',
-   description: 'La storia di Buenos Aires sta scritta nel suo elenco telefonico',
-   url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg'
-  },
-  {
-   title: 'Colombia',
-   description: 'In Colombia avere le allucinazioni è una normalità',
-   url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop'
+
+  mounted(){
+    
   }
-];
+
+}).mount('#app');
+
